7685c872eb3a9643ab78f77c322cdc640f093c93
Jadees <Jadees@10.40.41.33>
2018-05-28 18:02:41 -0400

    modifique asd

--- asd/src/asd/Asd.java
+++ asd/src/asd/Asd.java
@@ -15,7 +15,7 @@ public class Asd {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        private int algo;
     }
     
 }
